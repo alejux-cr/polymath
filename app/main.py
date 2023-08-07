4c16fe187a776b72af030e9fee0069a47b7e1255
Alejandro Castillo <alejuxcr@gmail.com>
2023-08-07 00:54:14 -0600

    Architectural updates STILL WIP

--- app/main.py
+++ app/main.py
@@ -1,92 +1,16 @@
-import os
-import openai
-from dotenv import load_dotenv, find_dotenv
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from langchain.memory import ConversationTokenBufferMemory
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.memory import ConversationSummaryBufferMemory
 
-from langchain.output_parsers import ResponseSchema
-from langchain.output_parsers import StructuredOutputParser
+
+from langchain.chains import ConversationChain
 from langchain.prompts import ChatPromptTemplate
 
-_ = load_dotenv(find_dotenv()) # read local .env file
-openai.api_key = os.environ['OPENAI_API_KEY']
 
-gift_schema = ResponseSchema(name="gift",
-                             description="Was the item purchased\
-                             as a gift for someone else? \
-                             Answer True if yes,\
-                             False if not or unknown.")
-delivery_days_schema = ResponseSchema(name="delivery_days",
-                                      description="How many days\
-                                      did it take for the product\
-                                      to arrive? If this \
-                                      information is not found,\
-                                      output -1.")
-price_value_schema = ResponseSchema(name="price_value",
-                                    description="Extract any\
-                                    sentences about the value or \
-                                    price, and output them as a \
-                                    comma separated Python list.")
+def main():
+    pass
 
-response_schemas = [gift_schema,
-                    delivery_days_schema,
-                    price_value_schema]
+if __name__ == '__main__':
+    main()
 
-output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
-format_instructions = output_parser.get_format_instructions()
 
-customer_review = """\
-This leaf blower is pretty amazing.  It has four settings:\
-candle blower, gentle breeze, windy city, and tornado. \
-It arrived in two days, just in time for my wife's \
-anniversary present. \
-I think my wife liked it so much she was speechless. \
-So far I've been the only one using it, and I've been \
-using it every other morning to clear the leaves on our lawn. \
-It's slightly more expensive than the other leaf blowers \
-out there, but I think it's worth it for the extra features.
-"""
-
-review_template = """\
-For the following text, extract the following information:
-
-gift: Was the item purchased as a gift for someone else? \
-Answer True if yes, False if not or unknown.
-
-delivery_days: How many days did it take for the product \
-to arrive? If this information is not found, output -1.
-
-price_value: Extract any sentences about the value or price,\
-and output them as a comma separated Python list.
-
-Format the output as JSON with the following keys:
-gift
-delivery_days
-price_value
-
-text: {text}
-"""
-
-review_template_2 = """\
-For the following text, extract the following information:
-
-gift: Was the item purchased as a gift for someone else? \
-Answer True if yes, False if not or unknown.
-
-delivery_days: How many days did it take for the product\
-to arrive? If this information is not found, output -1.
-
-price_value: Extract any sentences about the value or price,\
-and output them as a comma separated Python list.
-
-text: {text}
-
-{format_instructions}
-"""
 
 # prompt_template = ChatPromptTemplate.from_template(review_template)
 # messages = prompt_template.format_messages(text=customer_review)
@@ -99,8 +23,8 @@ messages = prompt.format_messages(text=customer_review,
 # To control the randomness and creativity of the generated
 # text by an LLM, use temperature = 0.0
 chat = ChatOpenAI(temperature=0.0)
-# memory = ConversationBufferMemory()
 
+# memory = ConversationBufferMemory()
 memory = ConversationBufferWindowMemory(k=10)
 memory = ConversationTokenBufferMemory(llm=chat, max_token_limit=30)
 memory = ConversationSummaryBufferMemory(llm=chat, max_token_limit=100)
